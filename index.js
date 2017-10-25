8faa442f6d6d77c3287eae83d55d3f42cbe58b70
Scott Atkins <satkins16@gmail.com>
2017-10-25 23:43:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,10 +20,10 @@ function distanceTravelledInFeet (start, finish) {
 
 function calculatesFarePrice (start, finish) {
   const distance = distanceTravelledInFeet(start, finish)
-  
+
   if (distance <= 400) {
     return 0
-  } else if (distance > 400 && distance <= 2000 ) {
-    
+  } else if (distance > 400 && distance <= 2000) {
+
   }
 }
